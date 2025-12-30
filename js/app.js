4ded4f290c3382f53484bbf431bc5ccccaa5be23
SAURABH MONDAL <saurabh.jopl@gmail.com>
2025-12-30 14:12:50 +0530

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import { initUploader } from "./ui/uploader.js";
+import { initTabs } from "./ui/tabs.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initUploader();
+  initTabs();
+});
